07fa182212164a0b009cea8792ef0b90ab770af5
Jan Tekautschitz <jan.tekautschitz@rub.de>
2021-06-09 19:53:07 +0200

    Automated scanning + result output

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'dart:async';
 import 'dart:developer' as dev;
+import 'dart:io' as io;
 
 void main() {
   runApp(MyApp());
@@ -20,6 +22,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+var allRssiMap = new Map<DeviceIdentifier, String>();
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -35,32 +39,111 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   final FlutterBlue flutterBlue = FlutterBlue.instance;
-  final List<BluetoothDevice> deviceList = [];
-  final rssiMap = new Map<DeviceIdentifier, int>();
+  List<BluetoothDevice> deviceList = [];
+  var currentRssiMap = new Map<DeviceIdentifier, int>();
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class MySecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Open L2CAP Channel"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class MyThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BLE Scanning Results"),
+      ),
+      body: SingleChildScrollView(
+        child: Expanded(
+          flex: 1,
+          child: Text(
+            _getAllRssiMapAsString(),
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+            maxLines: null,
+          ),
+        )
+      ),
+    );
+  }
+
+  _getAllRssiMapAsString(){
+    String info = '';
+    allRssiMap.forEach((k, v) => {
+      info += '\n $k \n\n $v \n\n --- \n',
+    });
+    return info;
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
+
+  TextEditingController tec = new TextEditingController();
+  Timer timer;
+  int scansSoFar = -2;
 
   _addDevice(final ScanResult result) {
     if (!widget.deviceList.contains(result.device)) {
       setState(() {
         widget.deviceList.add(result.device);
-        widget.rssiMap[result.device.id] = result.rssi;
+        widget.currentRssiMap[result.device.id] = result.rssi;
       });
+    }
+    if (allRssiMap.containsKey(result.device.id))
+      allRssiMap[result.device.id] += ' ' + result.rssi.toString();
+    else
+      allRssiMap[result.device.id] = result.rssi.toString();
+  }
+
+  _scanForDevices() {
+    int amountScans = int.parse(tec.text);
+    if (!amountScans.isNaN) {
+      _singleScan();
+      timer = Timer.periodic(Duration(seconds: 4), (Timer t) => _singleScan());
+    }
+  }
+
+  _singleScan() {
+    widget.deviceList = [];
+    widget.flutterBlue.scanResults.listen((List<ScanResult> results) {
+      for (ScanResult result in results)
+        _addDevice(result);
+    });
+    widget.flutterBlue.startScan(timeout: Duration(seconds: 3));
+    scansSoFar += 1;
+    if (int.parse(tec.text) == scansSoFar && scansSoFar >= 0) {
+      timer.cancel();
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => MyThirdPage()),
+      );
+      scansSoFar = -2;
     }
   }
 
   @override
   void initState() {
     super.initState();
-    widget.flutterBlue.scanResults.listen((List<ScanResult> results) {
-      for (ScanResult result in results)
-        _addDevice(result);
-    });
-    widget.flutterBlue.startScan();
   }
 
   ListView _buildListView() {
@@ -101,10 +184,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Column(
                   children: <Widget>[
                     Text(
-                      "RSSI: " + widget.rssiMap[device.id].toString(),
-                      style: widget.rssiMap[device.id] <= -100 ?
+                      "RSSI: " + widget.currentRssiMap[device.id].toString(),
+                      style: widget.currentRssiMap[device.id] <= -100 ?
                           TextStyle(color: Colors.red) :
-                        widget.rssiMap[device.id] <= -50 ?
+                        widget.currentRssiMap[device.id] <= -50 ?
                           TextStyle(color: Colors.yellow) :
                           TextStyle(color: Colors.green),
                     ),
@@ -125,19 +208,31 @@ class _MyHomePageState extends State<MyHomePage> {
               border: OutlineInputBorder(),
               hintText: 'Enter # Scans'
           ),
+          controller: tec,
+          keyboardType: TextInputType.number,
         ),
         TextButton(
           onPressed: () {
-            print('button 1 test');
+            _scanForDevices();
           },
           child: const Text('Start BLE Scanning'),
         ),
         TextButton(
           onPressed: () {
-            print('button 2 test');
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MySecondPage()),
+            );
           },
           child: const Text('Open L2CAP Channel'),
         ),
+        const Divider(
+          height: 5,
+          thickness: 1,
+          indent: 0,
+          endIndent: 0,
+        ),
+        SizedBox(height: 15),
         ...containers,
       ],
     );
